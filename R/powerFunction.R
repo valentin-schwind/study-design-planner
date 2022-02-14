7584eacb43728456f9650d813b2082aa673ef442
Valentin Schwind <mail@vali.de>
2022-02-14 21:17:26 +0100

    imports

--- R/powerFunction.R
+++ R/powerFunction.R
@@ -1,12 +1,17 @@
-powerFunction <- function(studyDesign, N, means, SD, labels, wcorr, alpha) {
+powerFunction <- function(studyDesign, N, means, SD) {
+
   if (!requireNamespace("Superpower", quietly = TRUE)) {
     stop(
       "Package \"Superpower\" must be installed to use this function.",
       call. = FALSE
     )
   }
-  invisible(Superpower::design_result <- ANOVA_design(design = studyDesign, n = N,  mu = means, sd = SD, labelnames = labels, r = wcorr, plot = F))
-  invisible(Superpower::exact_result <- ANOVA_exact(design_result, alpha_level = alpha))
-  return (list(min(exact_result$main_results$power),max(exact_result$main_results$power)))
 
+  design_result <- Superpower::ANOVA_design(design = studyDesign, n = N,  mu = means, sd = SD, r = 0.5, plot = F)
+
+  exact_result <- Superpower::ANOVA_exact(design_result, alpha_level = 0.05)
+
+  invisible()
+
+  return (list(min(exact_result$main_results$power),max(exact_result$main_results$power)))
 }
