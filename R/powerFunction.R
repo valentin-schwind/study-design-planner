9765c26d5ac87f8b8f15a40b59e9fb2e1a3ca847
Valentin Schwind <mail@vali.de>
2022-02-14 20:55:24 +0100

    test

--- R/powerFunction.R
+++ R/powerFunction.R
@@ -1,19 +1,15 @@
+powerFunction <- function(studyDesign, N, means, SD) {
 
-
-powerFunction <- function(studyDesign, N, means, SD, labels, wcorr, alpha) {
   if (!requireNamespace("Superpower", quietly = TRUE)) {
     stop(
       "Package \"Superpower\" must be installed to use this function.",
       call. = FALSE
     )
   }
-  round_df <- function(df, digits = 3) {
-    nums <- vapply(df, is.numeric, FUN.VALUE = logical(1))
-    df[,nums] <- round(df[,nums], digits = digits)
-    (df)
-  }
 
-  invisible(capture.output(design_result <- Superpower::ANOVA_design(design = studyDesign, n = N,  mu = means, sd = SD, labelnames = labels, r = wcorr, plot = F)))
-  invisible(capture.output(exact_result  <- Superpower::ANOVA_exact(design_result, alpha_level = alpha)))
-  exact_result$main_results
+  design_result <- Superpower::ANOVA_design(design = studyDesign, n = N,  mu = means, sd = SD, r = 0.5, plot = F)
+
+  exact_result <- Superpower::ANOVA_exact(design_result, alpha_level = 0.05)
+
+  return (list(min(exact_result$main_results$power),max(exact_result$main_results$power)))
 }
