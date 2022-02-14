251e35173ef4b180b3bd04993d15f8dfb78e8356
Valentin Schwind <mail@vali.de>
2022-02-14 03:21:09 +0100

    Update

--- R/powerFunction.R
+++ R/powerFunction.R
@@ -1,5 +1,4 @@
 library(Superpower)
-
 minMaxPower <- function(studyDesign, N, means, SD) { 
     design_result <- ANOVA_design(design = studyDesign, n = N,  mu = means, sd = SD, r = 0.5, plot = F)
     exact_result <- ANOVA_exact(design_result, alpha_level = 0.05))
