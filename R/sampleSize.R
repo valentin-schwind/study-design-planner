6a7ee44f87cc06f599b89556c9785caff58404ba
Valentin Schwind <mail@vali.de>
2022-02-18 00:41:20 +0100

    power

--- R/sampleSize.R
+++ R/sampleSize.R
@@ -1,3 +1,9 @@
 sampleSize <- function(cohensf, groups, powerlevel, alpha) {
-  return(pwr.anova.test(f=cohensf,k=groups,power=powerlevel,sig.level=alpha)$n)
+  if (!requireNamespace("pwr", quietly = TRUE)) {
+    stop(
+      "Package \"pwr\" must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+  return(pwr::pwr.anova.test(f=cohensf,k=groups,power=powerlevel,sig.level=alpha)$n)
 }
